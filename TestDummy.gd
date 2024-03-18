741b5303bf7b656aa580a097236c6af70d993a27
Excaliabure <106698782+Excaliabure@users.noreply.github.com>
2024-03-17 19:44:47 -0700

    first time on frame
    y

--- TestDummy.gd
+++ TestDummy.gd
@@ -6,10 +6,9 @@ var speed = 100  # Adjust the speed of movement
 var target_position = Vector3()  # Target position for the AI to move towards
 var collision_distance = 50  # Distance to detect obstacles
 
-
+@onready var  ray_cast = $RayStart
 
 func _ready():
-	
 	pass  # Initialization code goes here
 
 func _process(delta):
@@ -19,21 +18,18 @@ func _process(delta):
 	### If there's an obstacle, adjust movement vector
 	#if obstacle_direction != Vector3.ZERO:
 		#movement_vector = movement_vector.slide(obstacle_direction)
-		
+	transform.origin.x += 0.1;
+	print(ray_cast.global_transform) 
+	
 	#transform.origin.x += 0.01;
 	## Move the AI
 	move_and_slide()
 
 func check_for_obstacle():
 	# Cast ray in the direction of movement to detect obstacles
-	var ray_cast = $RayCast3D
-	#ray_cast.cast_to = (target_position - position).normalized() * collision_distance
-	#ray_cast.force_raycast_update()
+	
+	
 	#
 	## If an obstacle is detected, return the direction to slide away from
-	#if ray_cast.is_colliding():
-		#var collision_normal = ray_cast.get_collision_normal()
-		#return collision_normal
-	#else:
-		#return Vector3.ZERO  # No obstacle detected
+	
 	pass
